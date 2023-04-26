08bbd672dd5dc40f32e9db8619857760d9bfd45c
TheDanishMexican <danieljchristjansen@gmail.com>
2023-04-26 22:08:51 +0200

    added another function

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ window.addEventListener("load", start);
 
 function start() {
   console.log("JavaScript is working");
+  anotherFunction();
+}
+
+function anotherFunction(); {
+    console.log("This is another function");
 }
